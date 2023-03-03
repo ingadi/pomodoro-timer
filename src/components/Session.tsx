55542b7c6bdf9e802a8c419a338a0f74e8945d58
ingadi <axelingadi@gmail.com>
2023-03-03 23:21:23 +0300

    feature: toggle input visibility

--- src/components/Session.tsx
+++ src/components/Session.tsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import styles from "./Session.module.css";
+import { MdCheckCircleOutline, MdCancel } from "react-icons/md";
 
 export default function Session({ duration, type, isActive }: Props) {
+  const [timerValue, setTimerValue] = useState(duration);
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <article className={`${styles.session} ${isActive ? styles.active : ""}`}>
-      {/* <span>{duration}</span> */}
-      <input className={styles.input} value={duration} type="text" />
+      {!isEditing && <span onClick={() => setIsEditing(true)}>{duration}</span>}
+      {isEditing && (
+        <>
+          <input
+            name="timer"
+            className={styles.input}
+            value={timerValue}
+            type="text"
+            onChange={(e) => setTimerValue(e.target.value)}
+            autoFocus={true}
+          />
+          <div className={styles.controls}>
+            <button className={styles.button}>
+              <MdCheckCircleOutline />
+            </button>
+            <button
+              className={styles.button}
+              onClick={() => {
+                setIsEditing(false);
+                setTimerValue(duration);
+              }}
+            >
+              <MdCancel />
+            </button>
+          </div>
+        </>
+      )}
     </article>
   );
 }
