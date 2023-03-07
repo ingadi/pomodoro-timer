6fe98740e336e515a71c47999e67e0ce3bcdf60b
ingadi <axelingadi@gmail.com>
2023-03-07 23:58:55 +0300

    feat: show/hide input and cycle count goal text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,35 @@
 import styles from "./Header.module.css";
 import { GrAchievement } from "react-icons/gr";
+import { useState } from "react";
 
 export function Header({ cycleCount, cycleCountGoal, type }: Props) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleUpdateCycleGoal() {
+    console.log("hi");
+  }
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>{type}</h1>
       <label className={styles.cycles}>
         {cycleCount >= cycleCountGoal && <GrAchievement />} {cycleCount} /{" "}
-        <span>
-          <b className={styles["cycle-goal"]}>
-            {cycleCountGoal === Infinity ? "Ꝏ" : cycleCountGoal}
-          </b>{" "}
-          cycles
-        </span>
-        {/* <span>
-        <input type="text" />
-        <button></button>
-        <button></button>
-      </span> */}
+        {!isEditing && (
+          <span onClick={() => setIsEditing(true)}>
+            <b className={styles["cycle-goal"]}>
+              {cycleCountGoal === Infinity ? "Ꝏ" : cycleCountGoal}
+            </b>{" "}
+            cycles
+          </span>
+        )}
+        {isEditing && (
+          <span>
+            <input type="text" />
+            {/* extract as controls */}
+            <button onClick={() => handleUpdateCycleGoal()}>Save</button>
+            <button onClick={() => setIsEditing(false)}>Cancel</button>
+          </span>
+        )}
       </label>
     </header>
   );
