27a998c0907130a43f85936ed5338060e8bae691
ingadi <axelingadi@gmail.com>
2023-03-25 22:09:15 +0300

    feat: track daily short and long break sessions

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -4,31 +4,40 @@ import styles from "./Pomodoro.module.css";
 
 export default function Pomodoro({ count, goal }: Props) {
   return (
-    <div className={styles.pomos}>
-      {count >= goal && goal !== 0 && (
-        <AiOutlineTrophy
-          title="Daily goal achieved"
-          className={`${styles.trophy} ${styles.tooltip} ${
-            count === goal ? styles.wiggle : ""
-          }`}
-        />
-      )}
-      <span
-        className={`${styles["work-interval-count"]} ${styles.tooltip}`}
-        title="Today's completed pomodoros"
-      >
-        {count}
-      </span>
-      /
-      {goal === 0 ? (
-        <BsInfinity className={styles.tooltip} title="Daily goal not set" />
-      ) : (
-        <span className={styles.tooltip} title="Daily goal">
-          {goal}
+    <details className={styles.pomodoro}>
+      <summary className={styles.wrapper}>
+        <span className={styles.summary}>
+          {count >= goal && goal !== 0 && (
+            <AiOutlineTrophy
+              title="Daily goal achieved"
+              className={`${styles.trophy} ${styles.tooltip} ${
+                count === goal ? styles.wiggle : ""
+              }`}
+            />
+          )}
+          <span
+            className={`${styles["work-interval-count"]} ${styles.tooltip}`}
+            title="Today's completed pomodoros"
+          >
+            {count}
+          </span>
+          /
+          {goal === 0 ? (
+            <BsInfinity className={styles.tooltip} title="Daily goal not set" />
+          ) : (
+            <span className={styles.tooltip} title="Daily goal">
+              {goal}
+            </span>
+          )}
+          pomos
         </span>
-      )}
-      pomos
-    </div>
+      </summary>
+      <p className={styles.intervals}>
+        <span className={styles.interval}>1 Pomodoro</span>
+        <span className={styles.interval}>1 Short break</span>
+        <span className={styles.interval}>0 Long break</span>
+      </p>
+    </details>
   );
 }
 
