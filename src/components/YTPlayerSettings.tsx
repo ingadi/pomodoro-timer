3b67e0116746959780fc9a3345f7461d201358f8
ingadi <axelingadi@gmail.com>
2023-03-31 22:00:27 +0300

    feat: create YTPlayerSettings schema

--- src/components/YTPlayerSettings.tsx
+++ src/components/YTPlayerSettings.tsx
@@ -2,33 +2,46 @@ import { z } from "zod";
 import Form from "@components/Form";
 import styles from "./Settings.module.css";
 
-export default function YTSettings() {
-  return <></>;
+export default function YTPlayerSettings({ onDone, onUpdate }: Props) {
+  const formControls = (
+    <div className={styles.controls}>
+      <div className={styles.primary}>
+        <button className={styles.button} onClick={onDone} type="button">
+          Cancel
+        </button>
+        <button
+          className={`${styles.button} ${styles["button-primary"]}`}
+          type="submit"
+        >
+          Save changes
+        </button>
+      </div>
+    </div>
+  );
+
+  return (
+    <Form
+      schema={FormSchema}
+      onSubmit={(data: z.infer<typeof FormSchema>) => {
+        onUpdate();
+        onDone();
+      }}
+      renderAfter={() => formControls}
+    />
+  );
 }
 
 const FormSchema = z.object({
-  "playlist id": z.string().describe(`Playlist ID`),
+  "playlist id": z
+    .string()
+    .describe(`Playlist ID // ${"PLt7bG0K25iXjy1L7Wpf6jgeEvMlwpNpqF"}`)
+    .regex(/^PL.{32}$/),
 });
 
-// PLT6yxVwBEbi22rbp2Mve4yEJVpSJFcC9g
-// PLcMLmU-CYwGdOF5632N3feUuFeMcexytk
-// PLBsP89CPrMeOpKhXiKyXg8AjiConuTXvI
-// PLflqtq8EOGAJjAm4Y1E-GtCkWVSPlZ1gf
-// PLYeOyMz9C9kZVnxBFqsYdh0PAs_kNR7mn
-// PLYeOyMz9C9kaI5rZupjsj3d4Dkiky3y-e
+type Props = {
+  // config: Config;
+  onUpdate: () => void;
+  onDone: () => void;
+};
 
-const id1 = "PLT6yxVwBEbi22rbp2Mve4yEJVpSJFcC9g";
-const id2 = "PLcMLmU-CYwGdOF5632N3feUuFeMcexytk";
-const id3 = "PLBsP89CPrMeOpKhXiKyXg8AjiConuTXvI";
-const id4 = "PLflqtq8EOGAJjAm4Y1E-GtCkWVSPlZ1gf";
-const id5 = "PLYeOyMz9C9kZVnxBFqsYdh0PAs_kNR7mn";
-const id6 = "PLYeOyMz9C9kaI5rZupjsj3d4Dkiky3y-e";
-
-const regexPattern = /^PL.{32}$/;
-
-console.log(regexPattern.test(id1)); // true
-console.log(regexPattern.test(id2)); // false
-console.log(regexPattern.test(id3)); // true
-console.log(regexPattern.test(id4)); // true
-console.log(regexPattern.test(id5)); // true
-console.log(regexPattern.test(id6)); // true
+// const regexPattern = /^PL.{32}$/;
